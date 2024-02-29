0a9c04d7bee25c85b78611f5ce45e35b47cd5815
Oliver Pan <2216991777@qq.com>
2024-02-29 01:54:34 -0700

    Refactor pagination component

--- components/site/pagination.tsx
+++ components/site/pagination.tsx
@@ -13,53 +13,80 @@ export type PaginationProps = {
     totalPages: number;
 };
 
-function SitePagination({ currentPage, totalPages }: PaginationProps) {
+const SitePagination = ({ currentPage, totalPages }: PaginationProps) => {
+    const renderPageNumbers = () => {
+        let components = [];
+
+        if (currentPage > 1) {
+            components.push(
+                <PaginationItem key="prev">
+                    <PaginationPrevious href={`?page=${currentPage - 1}`} />
+                </PaginationItem>,
+            );
+        }
+
+        if (currentPage <= 3) {
+            components.push(renderPageItem(1));
+        }
+        // Always show the first page
+        if (currentPage > 3) {
+            components.push(renderPageItem(1));
+            components.push(
+                <PaginationItem key="ellipsis1">
+                    <PaginationEllipsis />
+                </PaginationItem>,
+            );
+        }
+
+        // Previous, current, and next page numbers
+        const startPage = Math.max(2, currentPage - 1);
+        const endPage = Math.min(totalPages, currentPage + 1);
+        for (let i = startPage; i <= endPage; i++) {
+            components.push(renderPageItem(i));
+        }
+
+        // Always show the last page
+        if (currentPage < totalPages - 2) {
+            components.push(
+                <PaginationItem key="ellipsis2">
+                    <PaginationEllipsis />
+                </PaginationItem>,
+            );
+            components.push(renderPageItem(totalPages));
+        }
+
+        if (currentPage < totalPages) {
+            components.push(
+                <PaginationItem key="next">
+                    <PaginationNext href={`?page=${currentPage + 1}`} />
+                </PaginationItem>,
+            );
+        }
+
+        return components;
+    };
+
+    const renderPageItem = (pageNumber: number) => (
+        <PaginationItem
+            key={pageNumber}
+            className={
+                currentPage === pageNumber ? 'underline underline-offset-2' : ''
+            }
+        >
+            <PaginationLink
+                href={`?page=${pageNumber}`}
+                aria-label={`Go to page ${pageNumber}`}
+            >
+                {pageNumber}
+            </PaginationLink>
+        </PaginationItem>
+    );
+
     return (
-        <Pagination className="mt-8">
-            <PaginationContent>
-                {currentPage > 1 && (
-                    <PaginationItem aria-disabled={currentPage === 1}>
-                        <PaginationPrevious
-                            href={{
-                                query: { page: currentPage - 1 },
-                            }}
-                        />
-                    </PaginationItem>
-                )}
-                {/* Previous Page Link - Only show if current page is not the first page */}
-                {currentPage > 1 && (
-                    <PaginationItem>
-                        <PaginationLink href={`?page=${currentPage - 1}`}>
-                            {currentPage - 1}
-                        </PaginationLink>
-                    </PaginationItem>
-                )}
-                {/* Current Page Link */}
-                <PaginationItem>
-                    <PaginationLink href={`?page=${currentPage}`}>
-                        {currentPage}
-                    </PaginationLink>
-                </PaginationItem>
-                {currentPage < totalPages && totalPages - currentPage > 1 && (
-                    <PaginationItem>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                )}
-                {currentPage <= totalPages - 1 && (
-                    <PaginationItem>
-                        <PaginationLink href={`?page=${totalPages}`}>
-                            {totalPages}
-                        </PaginationLink>
-                    </PaginationItem>
-                )}
-                {currentPage < totalPages && (
-                    <PaginationItem>
-                        <PaginationNext href={`?page=${currentPage + 1}`} />
-                    </PaginationItem>
-                )}
-            </PaginationContent>
+        <Pagination>
+            <PaginationContent>{renderPageNumbers()}</PaginationContent>
         </Pagination>
     );
-}
+};
 
 export default SitePagination;
